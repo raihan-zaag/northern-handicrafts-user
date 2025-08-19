4d8f0be8a941b8de070f48c84603d3139dbb0157
raihan-ops <tasnimraihan28@gmail.com>
2025-08-19 20:56:32 +0600

    fix(feat/auth):fixes eslint issue

--- README.md
+++ README.md
@@ -2,6 +2,9 @@ This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-
 
 ## Getting Started
 
+eslint fix 
+npx eslint . --config eslint.config.mjs
+
 First, run the development server:
 
 ```bash
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+/* eslint-disable import/no-anonymous-default-export */
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +11,29 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+export default [
+  js.configs.recommended,
+  ...compat.extends('next/core-web-vitals'),
+  {
+    files: ['**/*.{js,jsx}'],
+    rules: {
+      /* --------- Core rules --------- */
+      "no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-debugger": "error",
 
-export default eslintConfig;
+      /* --------- Import rules --------- */
+      // "import/no-unresolved": "error",
+      "import/no-duplicates": "error",
+
+      /* --------- React rules --------- */
+      "react/react-in-jsx-scope": "off", // Next.js handles React
+      "react/jsx-uses-react": "off",
+      "react/jsx-uses-vars": "error",
+      "react/prop-types": "off", // not needed unless you want prop-types
+    },
+  },
+];
